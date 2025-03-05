763afd8dc976dddd141a269c86e998694d3d1a51
Amund Oliver Josefsen <amundojo@gmail.com>
2025-03-05 14:50:23 +0100

    enhance .env loading with explicit path and debug logs - Added path.resolve for .env file - Added console logs for Env file path and jwt_secret.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const jobRoutes = require('./routes/jobRoutes');
 const authRoutes = require('./auth');
 
 // Load environment variables
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+console.log('Env file path:', path.resolve(__dirname, '.env'));
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -18,7 +21,6 @@ app.use('/api', jobRoutes);
 
 // Authentication Routes
 app.use('/api/auth', authRoutes);
-
 
 // Start server
 app.listen(port, () => {
